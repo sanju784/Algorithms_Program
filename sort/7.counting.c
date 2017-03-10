66767f772bdde635d5b21e4fe9fe73368e671bbc
sanjeev <sanjunitr51@gmail.com>
2017-03-10 13:15:33 +0900

    Counting Sort program

---
+++ sort/7.counting.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void countSort(int a[], int n) {
+	int i,j;
+	int b[n],c[n];
+	for(i = 0; i < n; i++)
+		c[i] = 0;
+	for(i=0; i < n; i++)
+		c[a[i]] = c[a[i]] + 1;
+	for(i = 1; i<n;i++)
+		c[i]=c[i]+c[i-1];
+	for(j=n; j>0;j--){
+		b[c[a[j]]-1] = a[j];
+		c[a[j]] = c[a[j]] - 1;
+	}
+	printf("\n\nThe sorted array is ");
+	for(i=0;i<n;i++)
+		printf(" %d", b[i]);
+}
+
+void main() {
+	int a[] = {1,4,1,2,7,5,2,0,0,0};
+    printf("\nThe array is ");
+    int i;
+	for(i=0;i<10;i++)
+		printf(" %d", a[i]);
+	countSort(a, 10);
+}
